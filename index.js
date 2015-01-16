7ad5d2243ee81b614f62d243b6ca516011a3a88f
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-01-16 23:31:10 +0100

    Bumped jsdom version

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = function(markup) {
     if (typeof document !== 'undefined') return
     var jsdom           = require("jsdom").jsdom
     global.document     = jsdom(markup || '')
-    global.window       = document.createWindow()
+    global.window       = document.parentWindow
     global.navigator    = window.navigator
     global.HTMLElement  = window.HTMLElement
     global.localStorage = require('localStorage')
