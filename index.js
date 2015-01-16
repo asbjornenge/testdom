750036111e077a034fdd4ce8d10a5ae5d2e40452
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-01-16 23:37:56 +0100

    Working solution

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-module.exports = function(markup) {
+module.exports = function(markup, additional_globals) {
     if (typeof document !== 'undefined') return
     var jsdom           = require("jsdom").jsdom
     global.document     = jsdom(markup || '')
     global.window       = document.parentWindow
     global.navigator    = window.navigator
     global.HTMLElement  = window.HTMLElement
-    global.localStorage = require('localStorage')
+    if (typeof additional_globals == 'object') Object.keys(additional_globals).forEach(function(global_key) {
+        global[global_key] = require(additional_globals[global_key])
+    })
 }
